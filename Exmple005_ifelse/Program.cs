0f1c45a10660c66d13d5065162ba37d0ecc1ea68
AksenovIvan <axenovivan7@gmail.com>
2023-10-06 10:45:44 +0300

    IFElSE Ex

---
+++ Exmple005_ifelse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "иван")
+{
+    Console.WriteLine("Ура!Это же Иван, тихо-тихо");
+}
+else
+{
+Console.Write ("Привет, ");
+Console.WriteLine (username);
+
+}
